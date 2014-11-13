64ba80c9418542343ed156061e6bb23071e21056
Daniel Merken <daniel.merken@gmail.com>
2014-11-13 15:13:31 -0800

    Update DriveTrain.cpp

--- DriveTrain.cpp
+++ DriveTrain.cpp
@@ -5,8 +5,6 @@ Robot::Robot():
   leftBackVic((uint32_t) PORT_DRIVE_VIC_LEFT_BACK), 
   rightFrontVic((uint32_t) PORT_DRIVE_VIC_RIGHT_FRONT), 
   rightBackVic((uint32_t) PORT_DRIVE_VIC_RIGHT_BACK), 
-  leftEnc((uint32_t) PORT_ENCODER_LEFT_A, (uint32_t) PORT_ENCODER_LEFT_B, true),
-	rightEnc((uint32_t) PORT_ENCODER_RIGHT_A, (uint32_t) PORT_ENCODER_RIGHT_B, false),
   leftFrontCon(0.1, 0.001, 0.0, &turretPot, &leftFrontVic), 
   leftBackCon(0.1, 0.001, 0.0, &turretPot, &leftBackVic), 
   rightFrontCon(0.1, 0.001, 0.0, &turretPot, &rightFrontVic), 
