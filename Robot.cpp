ada7e16a345df804e51d3ae5ef08a056b6a7c351
Daniel Merken <daniel.merken@gmail.com>
2014-11-13 16:19:40 -0800

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,7 +1,14 @@
 #include "Robot.h"
 
 Robot::Robot():
+  driveTrain(),
+  humanController(),
+  robotCommand(0)
+{
   
+}
 
-void Robot::update():
-  
+void Robot::update(){
+  humanController.update();
+  driveTrain.update();
+}
