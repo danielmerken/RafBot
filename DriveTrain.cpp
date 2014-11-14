916745e301bddad52a9eeb271ccf30bffcd698a2
Daniel Merken <daniel.merken@gmail.com>
2014-11-13 16:22:42 -0800

    Update DriveTrain.cpp

--- DriveTrain.cpp
+++ DriveTrain.cpp
@@ -1,21 +1,18 @@
 #include "DriveTrain.h"
+#include "RobotCommand.h"
 
 Robot::Robot(): 
-  leftFrontVic((uint32_t) PORT_DRIVE_VIC_LEFT_FRONT), 
-  leftBackVic((uint32_t) PORT_DRIVE_VIC_LEFT_BACK), 
-  rightFrontVic((uint32_t) PORT_DRIVE_VIC_RIGHT_FRONT), 
-  rightBackVic((uint32_t) PORT_DRIVE_VIC_RIGHT_BACK), 
-  leftFrontCon(0.1, 0.001, 0.0, &turretPot, &leftFrontVic), 
-  leftBackCon(0.1, 0.001, 0.0, &turretPot, &leftBackVic), 
-  rightFrontCon(0.1, 0.001, 0.0, &turretPot, &rightFrontVic), 
-  rightBackCon(0.1, 0.001, 0.0, &turretPot, &rightBackVic)
+  leftFrontVic((uint32_t) 5), 
+  leftBackVic((uint32_t) 6), 
+  rightFrontVic((uint32_t) 1), 
+  rightBackVic((uint32_t) 2), 
 {
   //...
 }
 
-void Robot::init() {
-
-}
-  
-
 void Robot::update() {
+  leftFrontVic.set(RobotCommand.getSpeed());
+  leftBackVic.set(RobotCommand.getSpeed());
+  rightFrontVic.set(RobotCommand.getSpeed());
+  rightBackVic.set(RobotCommand.getSpeed());
+}
