ab565642af5ff85af08814aaa33616876dd40179
Daniel Merken <daniel.merken@gmail.com>
2014-11-13 16:11:13 -0800

    Update DriveTrain.h

--- DriveTrain.h
+++ DriveTrain.h
@@ -3,24 +3,12 @@
 class DriveTrain {
 
   private:
-    DriveTrain driveTrain;
-    HumanController humanController;
-  
     //Victors
     Victor leftFrontVic;
     Victor leftBackVic;
     Victor rightFrontVic;
     Victor rightBackVic;
-    
-    //Encoders
-    Encoder rightEnc;
-    Encoder leftEnc;
-    
-    //PIDControllers
-    PIDController leftFrontCon;
-    PIDController leftBackCon;
-    PIDController rightFrontCon;
-    PIDController rightBackCon;
+
     
   public:
     DriveTrain();
