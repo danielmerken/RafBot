fe6a28edf2c2f0239e03d44c3df4a3fedeadceaf
Daniel Merken <daniel.merken@gmail.com>
2014-11-15 16:12:07 -0800

    Update HumanController.cpp

--- HumanController.cpp
+++ HumanController.cpp
@@ -1,14 +1,14 @@
 # include "HumanController.h"
 # include "RobotCommand.h"
 
-HumanController::HumanController():
+HumanController::HumanController(RobotCommand robotCommandPointer):
   joystick((uint32_t)1)
 {
-  
+  robotCommand=robotCommandPointer;
 }
 
 HumanController::update()
 {
-  RobotCommand.setSpeed(joystick.getY());
+  *robotCommand.setSpeed(joystick.getY());
 }
   
